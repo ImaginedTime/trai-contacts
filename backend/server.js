1037687b29048b4b36819fab2596581e2e81ef32
ImaginedTime <udayomsrivastava@kgpian.iitkgp.ac.in>
2025-10-24 19:02:17 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,6 @@ const { db, initDatabase, seedDatabase } = require('./database');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-const cors = require('cors');
-
 const corsOptions = {
   origin: ['http://localhost:5173'],
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
